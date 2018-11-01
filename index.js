b82e2b8d1cd17d7fc053959080bc9c3c8ce80cca
antjuntu <antjuntu@gmail.com>
2018-11-01 13:55:49 +0200

    build renamed to piblic

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 
 app.use(cors())
 app.use(bodyParser.json())
-app.use(express.static('build'))
+app.use(express.static('public'))
 
 let notes = [
   {
---
+++ public/precache-manifest.4ebc4bea492208e7eaea67493322b7d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fda21fb703e1cec8de18",
+    "url": "/static/js/main.fda21fb7.chunk.js"
+  },
+  {
+    "revision": "953eda7fd3949ef5aa0e",
+    "url": "/static/js/1.953eda7f.chunk.js"
+  },
+  {
+    "revision": "fda21fb703e1cec8de18",
+    "url": "/static/css/main.6792bb61.chunk.css"
+  },
+  {
+    "revision": "e18ec89e579a5482b0b098271a31795e",
+    "url": "/index.html"
+  }
+];
