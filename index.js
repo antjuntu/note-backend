37d209916505282f7c772547309d65fb37f6fdb9
antjuntu <antjuntu@gmail.com>
2018-11-05 14:01:21 +0200

    database for production created.

--- index.js
+++ index.js
@@ -43,14 +43,27 @@ app.get('/api/notes/:id', (request, response) => {
   Note
     .findById(request.params.id)
     .then(note => {
-      response.json(formatNote(note))
+      if (note) {
+        response.json(formatNote(note))
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(400).send({ error: 'malformatted id' })
     })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-  response.status(204).end()
+  Note
+    .findByIdAndRemove(request.params.id) 
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => {
+      response.status(400).send({ error: 'malformatted id' })
+    })
 })
 
 const generateId = () => {
@@ -73,8 +86,37 @@ app.post('/api/notes', (request, response) => {
 
   note
     .save()
-    .then(savedNote => {
-      response.json(formatNote(savedNote))
+    .then(formatNote)
+    .then(savedAndFormattedNote => {
+      response.json(savedAndFormattedNote)
+    })
+
+  // note
+  //   .save()
+  //   .then(savedNote => {
+  //     return formatNote(savedNote)
+  //   })
+  //   .then(savedAndFormattedNote => {
+  //     response.json(savedAndFormattedNote)
+  //   })
+})
+
+app.put('/api/notes/:id', (request, response) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important
+  }
+
+  Note
+    .findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(formatNote(updatedNote))
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(400).send({ error: 'malformatted id' })
     })
 })
 
--- models/note.js
+++ models/note.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const { url } = require('../db')
+const url = process.env.MONGODB_URI
 
 mongoose.connect(url, { useNewUrlParser: true })
 
